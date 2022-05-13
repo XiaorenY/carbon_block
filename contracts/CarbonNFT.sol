b61fc1a154343b12e88bc83923529ae041199d70
XiaorenY <sean950111@gmail.com>
2022-05-13 14:49:31 -0700

    feat: workflow diagram

--- contracts/CarbonNFT.sol
+++ contracts/CarbonNFT.sol
@@ -11,15 +11,17 @@ contract CarbonNFT is ERC721, Ownable, ERC721Burnable {
     mapping (uint256 => uint256) public tokenIdToExpectedCarbonCredit;
     // for each token id, burn at expire time
     mapping(uint256 => uint) public tokenIdToExpireTime;
+    mapping(uint256 => uint256) public tokenIdToFractionalToken;
 
     constructor () public ERC721 ("CarbonNFT", "Carbon") {}
 
-    function createCollectible(uint period, uint expectedCarbondCredit, string memory tokenURI) public returns (uint256) {
+    function createCollectible(uint256 expectedCarbondCredit, uint period, uint256 fractionalTokenNumber, string memory tokenURI) public returns (uint256) {
         uint256 newItemId = tokenCounter;
         _safeMint(msg.sender, newItemId);
         //_setTokenURI(newItemId, tokenURI);
         tokenIdToExpectedCarbonCredit[newItemId] = expectedCarbondCredit;
         tokenIdToExpireTime[newItemId] = block.timestamp + period;
+        tokenIdToFractionalToken[newItemId] = fractionalTokenNumber;
         tokenCounter = tokenCounter + 1;
         return newItemId;
     }
